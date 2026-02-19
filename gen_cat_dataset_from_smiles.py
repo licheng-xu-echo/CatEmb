a226d9dd568f18cd76f5a6d31373c45586ba4626
Li-Cheng Xu <licheng_xu@zju.edu.cn>
2026-02-19 06:43:28 +0000

    clean up

--- gen_cat_dataset_from_smiles.py
+++
@@ -1,4 +0,0 @@
-from catemb.data import CatDataset
-#dataset_old = CatDataset(root="../dataset/processed",name="lig_cat_dataset",seed=42,trunc=5000,save_smiles=True)
-dataset = CatDataset(root="../dataset/processed",name="lig_cat_dataset_new",seed=42,trunc=0,save_smiles=True)
-#dataset_no_smiles = CatDataset(root="../dataset/processed",name="lig_cat_dataset",seed=42,trunc=0,save_smiles=False)
